74add0f62c9e252d3bc5afc5e61879ce53474ed8
Jacob Watson <Jacob.Watson@SDC-World.com>
2020-07-12 13:43:34 -0400

    Added basic arithmetic cases

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -1,5 +1,34 @@
 package helloWorld;
 
 public class HelloWorld {
-
+	
+	public static void main(String[] args) {
+		int a = 42;
+		int b = 27;
+		int sum = a + b;
+		
+		// Print out the sum of the above numbers
+		System.out.println("a + b = " + sum);
+		
+		a = 87;
+		b = 18;
+		int difference = 0;
+		
+		// TODO: Print out the difference of the above numbers
+		System.out.println("a - b = " + difference);
+		
+		a = 3;
+		b = 23;
+		int product = 0;
+		
+		// TODO: Print out the product of the above numbers
+		System.out.println("a * b = " + product);
+		
+		a = 345;
+		b = 5;
+		int quotient = 0;
+		
+		// TODO: Print out the quotient of the above numbers
+		System.out.println("a / b = " + quotient);
+	}
 }
